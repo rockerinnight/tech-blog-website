05af9ce59cb17ad0432372453f3aa752e38cb4d0
Linh Doan Vu <doanvulinh3108@gmail.com>
2021-03-25 20:53:46 +0700

    generated screens, commons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/screens/home/home.component';
+import { SettingComponent } from './components/screens/setting/setting.component';
+import { ProfileComponent } from './components/screens/profile/profile.component';
+import { ArticleComponent } from './components/screens/article/article.component';
+import { SignUpComponent } from './components/screens/sign-up/sign-up.component';
+import { LoginComponent } from './components/screens/login/login.component';
+import { ButtonsComponent } from './components/commons/buttons/buttons.component';
+import { NavBarComponent } from './components/commons/nav-bar/nav-bar.component';
+import { NavComponent } from './components/commons/nav/nav.component';
+import { InputComponent } from './components/commons/input/input.component';
+import { TagComponent } from './components/commons/tag/tag.component';
+import { PaginationComponent } from './components/commons/pagination/pagination.component';
+import { FooterComponent } from './components/commons/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SettingComponent,
+    ProfileComponent,
+    ArticleComponent,
+    SignUpComponent,
+    LoginComponent,
+    ButtonsComponent,
+    NavBarComponent,
+    NavComponent,
+    InputComponent,
+    TagComponent,
+    PaginationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
